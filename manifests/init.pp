f30211c455e12082dc27e9994057703983635314
Bram Vogelaar <bram@inuits.eu>
2017-01-23 15:05:52 +0100

    adding basic check for default include

--- manifests/init.pp
+++ manifests/init.pp
@@ -371,6 +371,6 @@ class neo4j (
   anchor { 'neo4j::begin': } ->
   class{'::neo4j::install':} ->
   class{'::neo4j::config':} ~>
-  class{'::neo4j::service':}
+  class{'::neo4j::service':} ->
   anchor { 'neo4j::end': }
 }
