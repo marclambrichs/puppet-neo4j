c29f317a5496fd2517cabc2fdac704de50976edf
Bram Vogelaar <bram@inuits.eu>
2017-02-16 17:18:10 +0100

    moving to directly testing service class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -13,6 +13,155 @@ describe 'neo4j', :type => :class do
         it { should contain_class('neo4j::config').that_notifies('Class[neo4j::Service]') }
         it { should contain_class('neo4j::service').that_comes_before('Anchor[neo4j::end]') }
         it { should contain_anchor('neo4j::end') }
+
+        case facts[:osfamily]
+        when 'Debian'
+        case facts[:operatingsystemrelease]
+        when /(^7.*|^14\.04.*)/
+          it { should contain_service('neo4j').with({
+            'ensure'   => 'running',
+            'enable'   => true,
+            'name'     => 'neo4j',
+            'provider' => 'debian',
+            })
+          }
+        else
+          it { should contain_service('neo4j').with({
+            'ensure'   => 'running',
+            'enable'   => true,
+            'name'     => 'neo4j',
+            'provider' => 'systemd',
+            })
+          }
+        end
+        when 'RedHat'
+        case facts[:operatingsystemrelease]
+        when /^6.*/
+          it { should contain_service('neo4j').with({
+            'ensure'   => 'running',
+            'enable'   => true,
+            'name'     => 'neo4j',
+            'provider' => 'redhat',
+            })
+          }
+        else
+          it { should contain_service('neo4j').with({
+            'ensure'   => 'running',
+            'enable'   => true,
+            'name'     => 'neo4j',
+            'provider' => 'systemd',
+            })
+          }
+        end
+        end
+
+        it { should_not contain_file('/etc/init.d/neo4j') }
+        it { should_not contain_systemd__unit('neo4j.service') }
+      end
+
+      context 'with install method set to archive' do
+        let(:params) {
+          {
+            :install_method => 'archive',
+            :install_prefix => '/usr/share',
+            :version        => '3.1.1'
+          }
+        }
+
+        context 'with service_provider set to magic' do
+          let(:params) {
+            {
+              :install_method   => 'archive',
+              :install_prefix   => '/usr/share',
+              :service_provider => 'magic',
+              :version          => '3.1.1',
+            }
+          }
+          it { should raise_error(/\"magic\" does not match \"debian|init|redhat|systemd\"/)}
+        end
+
+        case facts[:osfamily]
+        when 'Debian'
+          case facts[:operatingsystemrelease]
+          when /(^7.*|^14\.04.*)/
+            it { should contain_file('/etc/init.d/neo4j') }
+            it { should_not contain_systemd__unit('neo4j.service') }
+
+            context 'with user and group set to test' do
+              let(:params) {
+                {
+                  :group          => 'test',
+                  :install_method => 'archive',
+                  :install_prefix => '/usr/share',
+                  :version        => '3.1.1',
+                  :user           => 'test',
+                }
+              }
+              it { should contain_file('/etc/init.d/neo4j').with({
+                'group' => 'test',
+                'owner' => 'test',
+                })
+              }
+            end
+
+            context 'with service_name set to test' do
+              let(:params) {
+                {
+                  :group          => 'test',
+                  :install_method => 'archive',
+                  :install_prefix => '/usr/share',
+                  :version        => '3.1.1',
+                  :user           => 'test',
+                  :service_name   => 'test',
+                }
+              }
+              it { should contain_file('/etc/init.d/test') }
+            end
+          else
+            it { should_not contain_file('/etc/init.d/neo4j') }
+            it { should contain_systemd__unit_file('neo4j.service') }
+          end
+        when 'RedHat'
+          case facts[:operatingsystemrelease]
+          when /^6.*/
+            it { should contain_file('/etc/init.d/neo4j') }
+            it { should_not contain_systemd__unit('neo4j.service') }
+
+            context 'with user and group set to test' do
+              let(:params) {
+                {
+                  :group          => 'test',
+                  :install_method => 'archive',
+                  :install_prefix => '/usr/share',
+                  :version        => '3.1.1',
+                  :user           => 'test',
+                }
+              }
+              it { should contain_file('/etc/init.d/neo4j').with({
+                  'group' => 'test',
+                  'owner' => 'test',
+                })
+              }
+            end
+
+            context 'with service_name set to test' do
+              let(:params) {
+                {
+                  :group          => 'test',
+                  :install_method => 'archive',
+                  :install_prefix => '/usr/share',
+                  :version        => '3.1.1',
+                  :user           => 'test',
+                  :service_name   => 'test',
+                }
+              }
+              it { should contain_file('/etc/init.d/test') }
+            end
+          else
+            it { should_not contain_file('/etc/init.d/neo4j') }
+            it { should contain_systemd__unit_file('neo4j.service') }
+          end
+        end
       end
     end
   end
--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -1,194 +1,268 @@
 require 'spec_helper'
-describe 'neo4j' do
-  on_os_under_test.each do |os, facts|
-    context "on #{os}" do
-      let(:facts) { facts }
-      context "service" do
+describe 'neo4j::service' do
+  let(:facts) {
+    {
+      :path => '/usr/bin',
+    }
+  }
+  let(:params) {
+    {
+      :group            => 'neo4j',
+      :install_method   => 'package',
+      :service_enable   => true,
+      :service_ensure   => 'running',
+      :service_name     => 'neo4j',
+      :service_provider => 'systemd',
+      :user             => 'neo4j',
+    }
+  }
 
-        context 'with defaults for all parameters' do
-            case facts[:osfamily]
-            when 'Debian'
-            case facts[:operatingsystemrelease]
-            when /(^7.*|^14\.04.*)/
-              it { should contain_service('neo4j').with({
-                  'ensure'   => 'running',
-                  'enable'   => true,
-                  'name'     => 'neo4j',
-                  'provider' => 'debian',
-                })
-              }
-            else
-              it { should contain_service('neo4j').with({
-                  'ensure'   => 'running',
-                  'enable'   => true,
-                  'name'     => 'neo4j',
-                  'provider' => 'systemd',
-                })
-              }
-            end
-            when 'RedHat'
-            case facts[:operatingsystemrelease]
-            when /^6.*/
-              it { should contain_service('neo4j').with({
-                  'ensure'   => 'running',
-                  'enable'   => true,
-                  'name'     => 'neo4j',
-                  'provider' => 'redhat',
-                })
-              }
-            else
-              it { should contain_service('neo4j').with({
-                  'ensure'   => 'running',
-                  'enable'   => true,
-                  'name'     => 'neo4j',
-                  'provider' => 'systemd',
-                })
-              }
-            end
-            end
+  it { should_not contain_file('/etc/init.d/neo4j') }
+  it { should_not contain_systemd__unit_file('neo4j.service') }
 
-            it { should_not contain_file('/etc/init.d/neo4j') }
-            it { should_not contain_systemd__unit('neo4j.service') }
-        end
+  context 'with install method set to archive and service_enable set to true' do
+    let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'archive',
+        :service_enable   => true,
+        :service_ensure   => 'running',
+        :service_name     => 'neo4j',
+        :service_provider => 'systemd',
+        :user             => 'neo4j',
+      }
+    }
 
-        context 'with install method set to archive' do
-            let(:params) {
-              {
-                :install_method => 'archive',
-                :install_prefix => '/usr/share',
-                :version        => '3.1.1'
-              }
-            }
-
-            context 'with service_provider set to magic' do
-                let(:params) {
-                  {
-                    :install_method   => 'archive',
-                    :install_prefix   => '/usr/share',
-                    :service_provider => 'magic',
-                    :version          => '3.1.1',
-                  }
-                }
-                it { should raise_error(/\"magic\" does not match \"debian|init|redhat|systemd\"/)}
-            end
-
-            case facts[:osfamily]
-            when 'Debian'
-              case facts[:operatingsystemrelease]
-              when /(^7.*|^14\.04.*)/
-                it { should contain_file('/etc/init.d/neo4j') }
-                it { should_not contain_systemd__unit('neo4j.service') }
-
-                context 'with user and group set to test' do
-                    let(:params) {
-                      {
-                        :group          => 'test',
-                        :install_method => 'archive',
-                        :install_prefix => '/usr/share',
-                        :version        => '3.1.1',
-                        :user           => 'test',
-                      }
-                    }
-                    it { should contain_file('/etc/init.d/neo4j').with({
-                        'group' => 'test',
-                        'owner' => 'test',
-                      })
-                    }
-                end
-
-                context 'with service_name set to test' do
-                  let(:params) {
-                    {
-                      :group          => 'test',
-                      :install_method => 'archive',
-                      :install_prefix => '/usr/share',
-                      :version        => '3.1.1',
-                      :user           => 'test',
-                      :service_name   => 'test',
-                    }
-                  }
-                  it { should contain_file('/etc/init.d/test') }
-                end
-              else
-                it { should_not contain_file('/etc/init.d/neo4j') }
-                it { should contain_systemd__unit_file('neo4j.service') }
-              end
-            when 'RedHat'
-              case facts[:operatingsystemrelease]
-              when /^6.*/
-                it { should contain_file('/etc/init.d/neo4j') }
-                it { should_not contain_systemd__unit('neo4j.service') }
-
-                context 'with user and group set to test' do
-                    let(:params) {
-                      {
-                        :group          => 'test',
-                        :install_method => 'archive',
-                        :install_prefix => '/usr/share',
-                        :version        => '3.1.1',
-                        :user           => 'test',
-                      }
-                    }
-                    it { should contain_file('/etc/init.d/neo4j').with({
-                        'group' => 'test',
-                        'owner' => 'test',
-                      })
-                    }
-                end
-
-                context 'with service_name set to test' do
-                  let(:params) {
-                    {
-                      :group          => 'test',
-                      :install_method => 'archive',
-                      :install_prefix => '/usr/share',
-                      :version        => '3.1.1',
-                      :user           => 'test',
-                      :service_name   => 'test',
-                    }
-                  }
-                  it { should contain_file('/etc/init.d/test') }
-                end
-              else
-                it { should_not contain_file('/etc/init.d/neo4j') }
-                it { should contain_systemd__unit_file('neo4j.service') }
-              end
-            end
-        end
-
-        context 'with service_enable set to false' do
-          let(:params) {
-            {
-              :service_enable => false,
-            }
-          }
-          it { should_not contain_service('neo4j') }
-        end
-
-        context 'with service_ensure set to stopped' do
-          let(:params) {
-            {
-              :service_ensure => 'stopped',
-            }
-          }
-          it { should contain_service('neo4j').with({
-              'ensure' => 'stopped',
-            })
-          }
-        end
-
-        context 'with service_name set to test' do
-          let(:params) {
-            {
-              :service_name => 'test',
-            }
-          }
-          it { should contain_service('neo4j').with({
-              'name' => 'test',
-            })
-          }
-        end
-      end
+    context 'with service_provider set to systemd' do
+      it { should_not contain_file('/etc/init.d/neo4j') }
+      it { should contain_systemd__unit_file('neo4j.service') }
     end
+
+    context 'with service_provider set to debian' do
+      let(:params) {
+        {
+          :group            => 'neo4j',
+          :install_method   => 'archive',
+          :service_enable   => true,
+          :service_ensure   => 'running',
+          :service_name     => 'neo4j',
+          :service_provider => 'debian',
+          :user             => 'neo4j',
+        }
+      }
+      it { should contain_file('/etc/init.d/neo4j') }
+      it { should_not contain_systemd__unit_file('neo4j.service') }
+    end
+
+    context 'with service_provider set to init' do
+      let(:params) {
+        {
+          :group            => 'neo4j',
+          :install_method   => 'archive',
+          :service_enable   => true,
+          :service_ensure   => 'running',
+          :service_name     => 'neo4j',
+          :service_provider => 'init',
+          :user             => 'neo4j',
+        }
+      }
+      it { should contain_file('/etc/init.d/neo4j') }
+      it { should_not contain_systemd__unit_file('neo4j.service') }
+    end
+
+    context 'with service_provider set to redhat' do
+      let(:params) {
+        {
+          :group            => 'neo4j',
+          :install_method   => 'archive',
+          :service_enable   => true,
+          :service_ensure   => 'running',
+          :service_name     => 'neo4j',
+          :service_provider => 'redhat',
+          :user             => 'neo4j',
+        }
+      }
+      it { should contain_file('/etc/init.d/neo4j') }
+      it { should_not contain_systemd__unit_file('neo4j.service') }
+    end
+
+    context 'with service_provider set to magic' do
+      let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'archive',
+        :service_enable   => true,
+        :service_ensure   => 'running',
+        :service_name     => 'neo4j',
+        :service_provider => 'magic',
+        :user             => 'neo4j',
+      }
+      }
+      it { should raise_error(Puppet::PreformattedError,/Service provider magic not supported/)}
+    end
+
+    context 'with user and group set to test' do
+      let(:params) {
+        {
+          :group            => 'test',
+          :install_method   => 'archive',
+          :service_enable   => true,
+          :service_ensure   => 'running',
+          :service_name     => 'neo4j',
+          :service_provider => 'init',
+          :user             => 'test',
+        }
+      }
+      it { should contain_file('/etc/init.d/neo4j').with({
+          'group' => 'test',
+          'owner' => 'test',
+        })
+      }
+    end
+
+    context 'with service_name set to test' do
+      let(:params) {
+        {
+          :group            => 'neo4j',
+          :install_method   => 'archive',
+          :service_enable   => true,
+          :service_ensure   => 'running',
+          :service_name     => 'test',
+          :service_provider => 'init',
+          :user             => 'neo4j',
+        }
+      }
+      it { should contain_file('/etc/init.d/test') }
+    end
+  end
+
+  context 'with service_enable set to true' do
+    it { should contain_service('neo4j') }
+  end
+
+  context 'with service_enable set to false' do
+    let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'package',
+        :service_enable   => false,
+        :service_ensure   => 'running',
+        :service_name     => 'neo4j',
+        :service_provider => 'systemd',
+        :user             => 'neo4j',
+      }
+    }
+    it { should_not contain_service('neo4j') }
+  end
+
+  context 'with service_ensure set to running' do
+    it { should contain_service('neo4j').with({
+        'ensure' => 'running',
+      })
+    }
+  end
+
+  context 'with service_ensure set to stopped' do
+    let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'package',
+        :service_enable   => true,
+        :service_ensure   => 'stopped',
+        :service_name     => 'neo4j',
+        :service_provider => 'systemd',
+        :user             => 'neo4j',
+      }
+    }
+    it { should contain_service('neo4j').with({
+        'ensure' => 'stopped',
+      })
+    }
+  end
+
+  context 'with service_name set to neo4j' do
+    it { should contain_service('neo4j').with({
+        'name' => 'neo4j',
+      })
+    }
+  end
+
+  context 'with service_name set to test' do
+    let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'package',
+        :service_enable   => true,
+        :service_ensure   => 'running',
+        :service_name     => 'test',
+        :service_provider => 'systemd',
+        :user             => 'neo4j',
+      }
+    }
+    it { should contain_service('neo4j').with({
+        'name' => 'test',
+      })
+    }
+  end
+
+  context 'with service_provider set to systemd' do
+    it { should contain_service('neo4j').with({
+        'provider' => 'systemd',
+      })
+    }
+  end
+
+  context 'with service_provider set to debian' do
+    let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'package',
+        :service_enable   => true,
+        :service_ensure   => 'running',
+        :service_name     => 'neo4j',
+        :service_provider => 'debian',
+        :user             => 'neo4j',
+      }
+    }
+    it { should contain_service('neo4j').with({
+        'provider' => 'debian',
+      })
+    }
+  end
+
+  context 'with service_provider set to init' do
+    let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'package',
+        :service_enable   => true,
+        :service_ensure   => 'running',
+        :service_name     => 'neo4j',
+        :service_provider => 'init',
+        :user             => 'neo4j',
+      }
+    }
+    it { should contain_service('neo4j').with({
+        'provider' => 'init',
+      })
+    }
+  end
+
+  context 'with service_provider set to redhat' do
+    let(:params) {
+      {
+        :group            => 'neo4j',
+        :install_method   => 'package',
+        :service_enable   => true,
+        :service_ensure   => 'running',
+        :service_name     => 'neo4j',
+        :service_provider => 'redhat',
+        :user             => 'neo4j',
+      }
+    }
+    it { should contain_service('neo4j').with({
+        'provider' => 'redhat',
+      })
+    }
   end
 end
